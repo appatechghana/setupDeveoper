e2f19d21307abfdbeb1904549b2f1c24941470a6
appatechghana <admin@appatechghana.com>
2017-06-27 13:46:20 +0000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from postGreAddEditDeleteCls import identifiers, sheet
-
-begin = 7
-# p = begin
-while True:
-    begin = identifiers(begin,"a","e","d")
-    # print begin
-    if (sheet.cell(column=5, row=begin).value == None):
-        break
